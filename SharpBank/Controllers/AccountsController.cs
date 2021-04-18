570afd01eae66f3293b5e9988e7f97b65bcf7e38
Batrana Teodor <batrina.teodor.w7w@student.ucv.ro>
2021-04-18 18:40:31 +0300

    SB-3: Accounts module: - Create AccountsController - Create Accounts and AddAccount Views

---
+++ SharpBank/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SharpBank.Models.Accounts;
+
+namespace SharpBank.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount(BankAccountFormModel bankAccountForm)
+        {
+            //TODO add create account after login service is implemented
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SharpBank/Models/Accounts/BankAccountFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpBank.Models.Accounts
+{
+    public class BankAccountFormModel
+    {
+        public string AccountType { get; set; }
+        public string Currency { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordToken { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CNP { get; set; }
+        public string CI { get; set; }
+    }
+}
