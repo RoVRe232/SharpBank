4cc350970e4517d652b9da824cfd14926f9683ef
Paraschivescu Maria <paraschivescumariacristiana@gmail.com>
2021-05-14 02:08:31 +0300

    SS-4: Transactions module: - Created unit tests for codec and registration

---
+++ AppLogic.Tests/EncryptorDecryptorTest.cs
@@ -0,0 +1,39 @@
+﻿using BankAPI.Entities;
+using BankAPI.Repositories.Interfaces;
+using BankAPI.Services;
+using BankAPI.Utilities;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SharpBank.Models;
+using SharpBank.Utils;
+
+namespace AppLogic.Tests
+{
+    [TestClass]
+    public class EncryptorDecryptorTest
+    {
+        private string initial;
+        private string encodedBase64;
+        private string decoded;
+        private const string key = "bce2ea2315a1916b14ca5898a4e4133b";
+
+        [TestInitialize]
+        public void InitializeTestData()
+        {
+            initial = "asdfghjhZZZ12344_@qewrttZ";
+            encodedBase64 = "";
+            decoded = "";
+        }
+
+        [TestMethod]
+        public void CodecTest()
+        {
+            encodedBase64 = Codec.EncryptString(key, initial);
+            decoded = Codec.DecryptString(key, encodedBase64);
+
+            Assert.AreEqual(initial, decoded);
+        }
+    }
+}
+
